80d709ade836a3daa17d75b21506fcf8ee80847c
彭胜 <pengsheng@xunlei.com>
2019-09-30 11:43:20 +0800

    csv文件处理

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/extrame/ole2 v0.0.0-20160812065207-d69429661ad7 // indirect
 	github.com/extrame/xls v0.0.1
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca
 	github.com/tealeg/xlsx v1.0.3
 	golang.org/x/text v0.3.2
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
--- processor_csv.go
+++ processor_csv.go
@@ -1,10 +1,16 @@
 package xlsx
 
 import (
+	"bytes"
 	"encoding/csv"
+	"io/ioutil"
+	"strings"
+	"github.com/saintfish/chardet"
+	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/encoding/unicode"
 	"golang.org/x/text/transform"
-	"os"
+
 )
 
 type processorCsv struct {
@@ -16,15 +22,27 @@ func newProcessorCsv () *processorCsv {
 }
 
 func (p *processorCsv) Read (name string, sheetIndex int) (data [][]string, err error) {
-	f, err := os.Open(name)
+	byteStr, err := ioutil.ReadFile(name)
 	if err != nil {
 		return nil,err
 	}
-	defer f.Close()
 
-	decoder := simplifiedchinese.GBK.NewDecoder()
-	r := transform.NewReader(f, decoder)
+	detector := chardet.NewTextDetector()
+	charset, err := detector.DetectBest(byteStr)
+	if err != nil {
+		panic(err)
+	}
 
-	cf := csv.NewReader(r)
+	r := bytes.NewReader(byteStr)
+	var decoder *encoding.Decoder
+	if strings.Contains(charset.Charset, "UTF") {
+		decoder = unicode.UTF8.NewDecoder()
+	} else {
+		decoder = simplifiedchinese.GBK.NewDecoder()
+
+	}
+	rd := transform.NewReader(r, decoder)
+
+	cf := csv.NewReader(rd)
 	return cf.ReadAll()
 }
